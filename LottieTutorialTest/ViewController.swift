6aea8abfa0c754b1a4748941d67c587b8c749358
eharrison <evandro@itsdayoff.com>
2020-05-13 17:36:13 +0800

    Initial Commit

---
+++ LottieTutorialTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieTutorialTest
+//
+//  Created by Evandro Harrison Hoffmann on 13/05/2020.
+//  Copyright © 2020 LottieFiles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
